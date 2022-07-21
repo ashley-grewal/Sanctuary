c5e262a58a9b041e18da62ad6c5b73e1220796e6
lucia <shadkhkdehjl>
2022-07-20 19:28:14 -0700

    added some code but not finished - lucia

---
+++ Sanctuary/BreathingExerciseViewController.swift
@@ -0,0 +1,61 @@
+//
+//  BreathingExerciseViewController.swift
+//  Sanctuary
+//
+//  Created by Lucia Lu on 2022-07-20.
+//
+import AVKit
+import AVFoundation
+import UIKit
+
+
+class BreathingExerciseViewController: UIViewController {
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated:Bool){
+        super.viewDidAppear(animated)
+    
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Sanctuary breathing", ofType: "m4v")!))
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = view.bounds
+        view.layer.addSublayer(layer)
+    
+        player.play()
+}
+    
+    
+//    private func playVideo() {
+//            guard let path = Bundle.main.path(forResource: "Sanctuary breathing", ofType:"m4v") else {
+//                debugPrint("Sanctuary breathing.m4v not found")
+//                return
+//            }
+//            let player = AVPlayer(url: URL(fileURLWithPath: path))
+//            let playerController = AVPlayerViewController()
+//            playerController.player = player
+//            present(playerController, animated: true) {
+//                player.play()
+   
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- Sanctuary/ViewController.swift
+++ Sanctuary/ViewController.swift
@@ -9,19 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var welcome: UILabel!
-    
-    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    var welcome = ["Welcome,", "欢迎,","Bienvenue,", "ਸੁਆਗਤ ਹੈ,", "ようこそ,","hoan nghênh,", "환영하다,", "willkommen,"]
+//    @objc func updateText() {
+//            if i >= quotes.count{
+//                timer?.invalidate()
+//                timer = nil
+//            }
+//
+//            i += 1
+//            quoteLabel.text = "\(quotes[i]) Task Completed"
+//        }
+    func changeWelcomeLabel (welcometext : String){
+        welcomeLabel?.text = welcometext
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: .curveLinear, animations: { [self] in
+            welcomeLabel?.text = welcometext
+        }, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+            
+    for _ in 0...7{
+//        var random = Int.random(in: 0..<7)
 
-        
+        changeWelcomeLabel(welcometext: "hiii")
+        welcomeLabel?.text = welcome[1]
+        }
     }
-
-
-    
 }
 
+    
